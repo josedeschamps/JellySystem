5b39b1ee39512e8ec5c89e10ca476d856538bb4a
Jose The Tech Monk <deschamps.jose001@gmail.com>
2019-12-10 23:16:24 -0500

    Saving all data

---
+++ JellyTest/Assets/Script/Spawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject Jelly;
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Spawn", 2f, 2f);
+	}
+	
+
+
+    void Spawn(){
+        Instantiate(Jelly, transform.position, Quaternion.identity);
+    }
+}
